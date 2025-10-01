af64e74a55907c4b7267eed8603babce9308fd14
Maram berrahal <berrahalmaram5@gmail.com>
2025-10-01 22:18:13 +0100

    here i changed a dirname

--- App.js
+++ App.js
@@ -2,21 +2,24 @@ import formRouter from "./Router/formRouter.js";
 import { config } from "dotenv";
 import express from "express";
 import AppRouter from "./Router/AppRouter.js";
-import {join} from "path";
+import { dirname, join } from "path";
 import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 
 config ();
 const Port = process.env.PORT || "7000";
-const _dirname = import.meta.dirname;
+
 
 const App = express();
 App.use(express.urlencoded({extended : true}));
 App.use(express.json());
 
 App.set("view engine","ejs");
-App.set("views", join(_dirname, "/views"));
-App.use(express.static(join(_dirname, "/public")));
+App.set("views", join(__dirname, "/views"));
+App.use(express.static(join(__dirname, "/public")));
 
 App.use("/api/App",formRouter);
 App.use("/",AppRouter);
